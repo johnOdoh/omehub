543555df9fe12177e0d8e976c87eb9e3b13a31ec
Johnny <chineduodoh4@gmail.com>
2025-10-28 05:35:01 +0100

    Embeded youtube links for videos on the home and about us pages

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -28,7 +28,7 @@
 
             <div class="col-lg-6 position-relative align-self-start order-lg-last order-first" data-aos="fade-up" data-aos-delay="200">
                 <img src="{{ asset('assets/img/story.jpg') }}" class="img-fluid" alt="">
-                <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox pulsating-play-btn"></a>
+                <a href="https://youtube.com/embed/3WeeLhKguEY" class="glightbox pulsating-play-btn"></a>
             </div>
 
             <div class="col-lg-6 content order-last order-lg-first" data-aos="fade-up" data-aos-delay="100">
--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -80,7 +80,7 @@
 
             <div class="col-lg-6 position-relative align-self-start order-lg-last order-first" data-aos="fade-up" data-aos-delay="200">
                 <img src="{{ asset('assets/img/about.jpg') }}" class="img-fluid" alt="">
-                <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox pulsating-play-btn"></a>
+                <a href="https://youtube.com/embed/YeOgHg6m1iM" class="glightbox pulsating-play-btn"></a>
             </div>
 
             <div class="col-lg-6 content order-last order-lg-first" data-aos="fade-up" data-aos-delay="100">
