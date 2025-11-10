07893bb0c3bab3b74f548e5637934554512b5207
Johnny <chineduodoh4@gmail.com>
2025-11-10 07:52:10 +0100

    added disqus plugin for post comments

--- resources/views/public/blog-single.blade.php
+++ resources/views/public/blog-single.blade.php
@@ -40,10 +40,31 @@
                         </div>
                     </div>
                 @else
-                    <img src="{{ asset('storage/'.$post->file) }}" alt="" class="img-fluid services-img">
+                    <img src="{{ asset('storage/'.$post->file) }}" alt="cover image" class="img-fluid services-img">
                 @endif
                 <h3>{{ $post->title }}</h3>
                 {!! $post->body !!}
+                <div class="mt-5">
+                    <div id="disqus_thread"></div>
+                    <script>
+                        /**
+                        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+                        /*
+                        var disqus_config = function () {
+                        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                        };
+                        */
+                        (function() { // DON'T EDIT BELOW THIS LINE
+                        var d = document, s = d.createElement('script');
+                        s.src = 'https://omehub.disqus.com/embed.js';
+                        s.setAttribute('data-timestamp', +new Date());
+                        (d.head || d.body).appendChild(s);
+                        })();
+                    </script>
+                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                </div>
             </div>
 
         </div>
